ef2f120fc5d245946603ed8affc756e23c1aba61
Nicolas CARREZ <nicolas.carrez@gmail.com>
2020-03-23 15:36:04 +0100

    Mysimpleapp update

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // imports
-const msaApp = require("../msa-app")
 const { join, relative, dirname } = require('path')
 const { promisify } = require('util')
 const fs = require('fs')
@@ -9,43 +8,49 @@ const glob = require('glob')
 const globPrm = promisify(glob)
 const readFilePrm = promisify(fs.readFile)
 
+const MsaApp = require("@mysimpleapp/msa-app/module")
+
+
 // init
 var html
 const init = async function(){
-	// get dmg elems
-	const files = await globPrm(join(__dirname, "static/mysimplegame/lib/*/drawmygame.json"))
-	var descs = {}
-	for(var file of files){
-		var desc = await readFilePrm(file, 'utf8')
-		desc = JSON.parse(desc)
-		if(!Array.isArray(desc)) desc = [desc]
-		for(var d of desc){
-			d.dir = dirname(relative(join(__dirname, "static/mysimplegame/lib"), file))
-			descs[d.key] = d
-		}
-	}
-	// parse index.html template
-	const template = await readFilePrm(join(__dirname, 'templates/index.html.tmpl'), "utf8")
-	mustache.parse(template)
-	html = mustache.render(template, {descs:JSON.stringify(descs)})
+        // get dmg elems
+        const files = await globPrm(join(__dirname, "static/mysimplegame/lib/*/drawmygame.json"))
+        var descs = {}
+        for(var file of files){
+                var desc = await readFilePrm(file, 'utf8')
+                desc = JSON.parse(desc)
+                if(!Array.isArray(desc)) desc = [desc]
+                for(var d of desc){
+                        d.dir = dirname(relative(join(__dirname, "static/mysimplegame/lib"), file))
+                        descs[d.key] = d
+                }
+        }
+        // parse index.html template
+        const template = await readFilePrm(join(__dirname, 'templates/index.html.tmpl'), "utf8")
+        mustache.parse(template)
+        html = mustache.render(template, {descs:JSON.stringify(descs)})
 }
 init()
 
-const Dmg = class extends msaApp.constructor {
 
-	initDefaultRouteMdw() {
-		this.app.get('/', (req, res, next) => {
-			res.setHeader('content-type', 'text/html')
-			res.send(html)
-		})
-	}
+// Dmg
+const Dmg = class extends MsaApp {
 
-	initEndMdws() {
-		this.app.get('/refresh', async (req, res, next) => {
-			await init()
-			res.sendStatus(200)
-		})
-	}
+  initDefaultRouteMdw() {
+    this.app.get('/', (req, res, next) => {
+      res.setHeader('content-type', 'text/html')
+      res.send(html)
+    })
+  }
+
+  initEndMdws() {
+    this.app.get('/refresh', async (req, res, next) => {
+      await init()
+      res.sendStatus(200)
+    })
+  }
 }
+
 
 module.exports = new Dmg()
