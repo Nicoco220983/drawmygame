8292d0c4cd314e3dcdb365f5fc688d601c43a659
Nicoco220983 <nicolas.carrez@gmail.com>
2024-06-26 11:58:39 +0200

    Reimplem

---
+++ server.mjs
@@ -0,0 +1,294 @@
+// const { floor, random } = Math
+
+import { join, dirname } from "path"
+// import fs from "fs"
+import { fileURLToPath } from "url"
+// import crypto from "crypto"
+import { networkInterfaces } from "os"
+// import path from "path"
+
+import express from "express"
+// import { WebSocketServer } from 'ws'
+
+// import Consts from './static/consts.mjs'
+
+const PROD = ((process.env.MULTISTAR_ENV || "").toLowerCase() === "production") ? true : false
+const PORT = process.env.MULTISTAR_PORT || (PROD ? 8080 : 3000)
+const DIRNAME = dirname(fileURLToPath(import.meta.url))
+
+// const games = initGames()
+
+
+class GameServer {
+
+  constructor() {
+    this.port = PORT
+    this.rooms = {}
+    this.initApp()
+  }
+
+  initApp() {
+    this.app = express()
+    this.app.use('/static', express.static('static'))
+    this.app.get('/', (req, res) => {
+      res.sendFile(join(DIRNAME, 'static/index.html'));
+    })
+
+    // this.app.get("/", (req, res) => {
+    //   res.redirect(`/r/${this.generateRoomId()}`)
+    // })
+
+    // this.app.get("/r/:roomId", (req, res) => {
+    //   const { roomId } = req.params
+    //   res.sendFile(join(DIRNAME, "static/room.html"))
+    // })
+
+    // this.app.get("/r/:roomId/p", (req, res) => {
+    //   const { roomId } = req.params
+    //   const room = this.rooms[roomId]
+    //   if(room === undefined) return res.sendStatus(404)
+    //   res.redirect(`/r/${roomId}/p/${room.generatePlayerId()}`)
+    // })
+
+    // this.app.get("/r/:roomId/p/:playerId", (req, res) => {
+    //   const { roomId } = req.params
+    //   if(this.rooms[roomId] === undefined) return res.sendStatus(404)
+    //   res.sendFile(join(DIRNAME, "static/joypad.html"))
+    // })
+
+    // this.app.get("/games", (req, res) => {
+    //   res.json({ games })
+    // })
+  }
+
+  serve() {
+    this.server = this.app.listen(this.port)
+    // this.startWebocketServer()
+  }
+
+  // startWebocketServer() {
+
+  //   this.websocketServer = new WebSocketServer({ server: this.server })
+
+  //   this.websocketServer.on('connection', ws => {
+
+  //     ws.id = this.generateClientId()
+  //     console.log(`Client '${ws.id}' connected`)
+    
+  //     ws.on('message', (data, isBinary) => {
+  //       const msg = isBinary ? data : data.toString()
+  //       const key = msg.substring(0, Consts.MSG_KEY_LENGTH)
+  //       const body = msg.substring(Consts.MSG_KEY_LENGTH)
+  //       if(key === Consts.MSG_KEYS.JOYPAD_INPUT) this.onJoypadInput(ws, body)
+  //       else if(key === Consts.MSG_KEYS.GAME_INPUT) this.onGameInput(ws, body)
+  //       else if(key === Consts.MSG_KEYS.GAME_STATE) this.onGameState(ws, body)
+  //       else if(key === Consts.MSG_KEYS.IDENTIFY_GAME) this.onIdentifyGame(ws, JSON.parse(body))
+  //       else if(key === Consts.MSG_KEYS.IDENTIFY_PLAYER) this.onIdentifyPlayer(ws, JSON.parse(body))
+  //       else if(key === Consts.MSG_KEYS.START_GAME) this.onStartGame(ws, JSON.parse(body))
+  //       else if(key === Consts.MSG_KEYS.DISCONNECT_PLAYER) this.onDisconnectPlayer(ws, body)
+  //       else console.warn("Unknown websocket key", key)
+  //     })
+    
+  //     ws.on('error', console.error)
+    
+  //     ws.on('close', () => {
+  //       console.log(`Client '${ws.id}' disconnected`)
+  //       this.onClientDeconnection(ws)
+  //     })
+  //   })
+  // }
+
+  // generateClientId() {
+  //   return crypto.randomBytes(8).toString("hex")
+  // }
+
+  // generateRoomId() {
+  //   let it = 1
+  //   while(true) {
+  //     const roomId = PROD ? floor(random() * 1000).toString() : it.toString()
+  //     if(this.rooms[roomId] === undefined) return roomId
+  //     it++
+  //   }
+  // }
+
+  // onIdentifyGame(ws, kwargs) {
+  //   ws.type = "game"
+  //   const roomId = kwargs.id
+  //   ws.room = this.rooms[roomId] = new Room(
+  //     roomId, ws
+  //   )
+  //   console.log(`Room '${roomId}' created`)
+  // }
+
+  // onIdentifyPlayer(ws, kwargs) {
+  //   if(!ws.room) {
+  //     // first player connection
+  //     ws.type = "player"
+  //     const { roomId, id: playerId } = kwargs
+  //     const room = this.rooms[roomId]
+  //     if(!room || room.closed) { ws.close(); return }
+  //     ws.room = room
+  //     room.playerWebsockets[ws.id] = ws
+  //     console.log(`Player '${ws.id}' connected to room '${roomId}' as '${playerId}'`)
+  //     ws.send(Consts.MSG_KEYS.IDENTIFY_PLAYER + JSON.stringify({
+  //       name: `Player${playerId}`,
+  //       color: "blue"
+  //     }))
+  //     if(room.gameKey) {
+  //       ws.send(Consts.MSG_KEYS.START_GAME + JSON.stringify({
+  //         gameKey: room.gameKey
+  //       }))
+  //       if(room.gameState) ws.send(Consts.MSG_KEYS.GAME_STATE + room.gameState)
+  //     }
+  //   } else {
+  //     // player chose its name and color
+  //     const { room } = ws
+  //     if(!room || room.closed) { ws.close(); return }
+  //     if(kwargs.name) ws.name = kwargs.name
+  //     if(kwargs.color) ws.color = kwargs.color
+  //     const msg = Consts.MSG_KEYS.SYNC_PLAYERS + JSON.stringify(room.exportPlayers())
+  //     room.sendToGame(msg)
+  //     room.sendToPlayers(msg)
+  //   }
+  // }
+
+  // onStartGame(ws, kwargs) {
+  //   const { gameKey } = kwargs
+  //   const { room } = ws
+  //   if(!room || room.closed) { ws.close(); return }
+  //   room.gameKey = gameKey
+  //   room.gameState = null
+  //   console.log(`Game of room '${room.id}' set to '${gameKey}'`)
+  //   room.sendToPlayers(Consts.MSG_KEYS.START_GAME + JSON.stringify({
+  //     gameKey
+  //   }))
+  // }
+
+  // onClientDeconnection(ws) {
+  //   const { room } = ws
+  //   if(!room || room.closed) { ws.close(); return }
+  //   if(ws.type === "game") {
+  //     room.closed = true
+  //     for(let ws of Object.values(room.playerWebsockets)) ws.close()
+  //     delete this.rooms[room.id]
+  //     console.log(`Room '${room.id}' closed`)
+  //   } else if(ws.type === "player") {
+  //     delete room.playerWebsockets[ws.id]
+  //     console.log(`Player '${ws.id}' left the room '${room.id}'`)
+  //     const msg = Consts.MSG_KEYS.SYNC_PLAYERS + JSON.stringify(room.exportPlayers())
+  //     room.sendToGame(msg)
+  //     room.sendToPlayers(msg)
+  //   }
+  // }
+
+  // onJoypadInput(ws, body) {
+  //   const { room } = ws
+  //   if(!room || room.closed) { ws.close(); return }
+  //   room.sendToGame(Consts.MSG_KEYS.JOYPAD_INPUT + ws.id + ':' + body)
+  // }
+
+  // onGameInput(ws, body) {
+  //   const { room } = ws
+  //   if(!room || room.closed) { ws.close(); return }
+  //   room.sendToPlayers(Consts.MSG_KEYS.GAME_INPUT + body)
+  // }
+
+  // onGameState(ws, body) {
+  //   const { room } = ws
+  //   if(!room || room.closed) { ws.close(); return }
+  //   room.gameState = body
+  //   room.sendToPlayers(Consts.MSG_KEYS.GAME_STATE + body)
+  // }
+
+  // onDisconnectPlayer(ws, playerId) {
+  //   const { room } = ws
+  //   if(!room || room.closed) { ws.close(); return }
+  //   const playerWs = room.playerWebsockets[playerId]
+  //   if(!playerWs) return
+  //   playerWs.close()
+  // }
+}
+
+
+// class Room {
+
+//   constructor(id, gameWs) {
+//     this.id = id
+//     this.numPlayer = 1
+//     this.gameWebsocket = gameWs
+//     this.playerWebsockets = {}
+//     this.gameKey = null
+//     this.gameState = null
+//   }
+
+//   generatePlayerId() {
+//     const res = this.numPlayer.toString()
+//     this.numPlayer += 1
+//     return res
+//   }
+
+//   sendToGame(msg) {
+//     this.gameWebsocket.send(msg)
+//   }
+
+//   sendToPlayers(msg) {
+//     for(const jpws of Object.values(this.playerWebsockets)) {
+//       jpws.send(msg)
+//     }
+//   }
+
+//   exportPlayers() {
+//     const res = {}
+//     const { playerWebsockets } = this
+//     for(const jpWs of Object.values(playerWebsockets)) {
+//       const { name, color } = jpWs
+//       if(!name) continue
+//       res[jpWs.id] = { name, color }
+//     }
+//     return res
+//   }
+// }
+
+
+// function initGames() {
+//   const games = {}
+//   for(const dirent of fs.readdirSync(path.join(DIRNAME, 'static/game'), { withFileTypes: true })) {
+//     try {
+//       if(!dirent.isSymbolicLink()) continue
+//       const gameRelPath = path.join(dirent.path, dirent.name)
+//       const confPath = path.join(fs.realpathSync(gameRelPath), '../multistar.json')
+//       const conf = JSON.parse(fs.readFileSync(confPath))
+//       games[conf.key] = conf
+//     } catch(err) {
+//       console.error(err)
+//     }
+//   }
+//   return games
+// }
+
+
+function getLocalIps() {
+  const nets = networkInterfaces();
+  const res = {}
+
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!res[name]) {
+              res[name] = [];
+            }
+            res[name].push(net.address);
+        }
+    }
+  }
+  return res
+}
+
+
+const gameServer = new GameServer()
+gameServer.serve()
+const localIp = Object.values(getLocalIps())[0]
+console.log(`Server started at: http://${localIp}:${PORT}`)
---
+++ static/utils.mjs
@@ -0,0 +1,228 @@
+const { assign } = Object
+
+import Two from './two.min.mjs'
+
+
+class Group extends Two.Group {
+    update(time) {
+        this.children.forEach(s => s.update && s.update(time))
+    }
+}
+
+
+class GameAudio extends Audio {
+  constructor(src, kwargs) {
+    super(src)
+    this.preload = "auto"
+    assign(this, kwargs)
+    this.oncanplaythrough = () => this.loaded = true
+  }
+  play(kwargs) {
+    this.loop = (kwargs && kwargs.loop) === true
+    super.play()
+  }
+  replay() {
+    this.pause()
+    this.currentTime = 0
+    this.play()
+  }
+}
+
+
+function addTo(group, obj) {
+  group.add(obj)
+  return obj
+}
+
+
+function urlAbsPath(relPath){
+  const url = new URL(relPath, import.meta.url)
+  return url.pathname
+}
+
+
+function fitTwoToEl(two, wrapperEl, kwargs) {
+
+    const { width, height } = two
+    const backgroundColor = (kwargs && kwargs.background) || "black"
+    const parentEl = wrapperEl.parentElement
+
+    wrapperEl.style.aspectRatio = `${width}/${height}`
+    function fillSpace() {
+        const fitToWidth = (width/height > parentEl.clientWidth/parentEl.clientHeight)
+        wrapperEl.style.width = fitToWidth ? "100%" : "auto"
+        wrapperEl.style.height = fitToWidth ? "auto" : "100%"
+    }
+    fillSpace()
+    window.addEventListener("resize", fillSpace)
+
+    two.appendTo(wrapperEl)
+    assign(two.renderer.domElement.style, {
+        width: "100%",
+        height: "100%",
+        backgroundColor,
+    })
+}
+
+
+function newPointer(two) {
+
+    const el = two.renderer.domElement
+
+    const pointer = {
+        isDown: false,
+        x: null,
+        y: null
+    }
+    function _updPointer(isDown, pos) {
+        const rect = el.getBoundingClientRect()
+        assign(pointer, {
+            isDown: isDown === null ? pointer.isDown : isDown,
+            x: pos ? (pos.clientX - rect.left) * two.width / rect.width : null,
+            y: pos ? (pos.clientY - rect.top) * two.height / rect.height : null,
+        })
+    }
+
+    el.addEventListener("mousemove", evt => _updPointer(null, evt))
+    el.addEventListener("touchmove", evt => _updPointer(true, evt.changedTouches[0]))
+    el.addEventListener("mousedown", evt => _updPointer(true, evt))
+    el.addEventListener("touchstart", evt => _updPointer(true, evt.changedTouches[0]))
+    document.addEventListener("mouseup", evt => _updPointer(false, null))
+    document.addEventListener("touchend", evt => {
+        if(evt.touches.length === 0) _updPointer(false, null)
+        else _updPointer(true, evt.touches[0])
+    })
+
+    return pointer
+}
+
+
+const Loads = []
+
+function addToLoads(obj) {
+    Loads.push(obj)
+    return obj
+}
+
+function checkAllLoadsDone() {
+    for(const o of Loads)
+        if(!o.loaded)
+            return false
+    Loads.length = 0
+    return true
+}
+
+function newCanvas(width, height, color) {
+    const canvas = document.createElement("canvas")
+    canvas.width = width
+    canvas.height = height
+    if(color) {
+        const ctx = canvas.getContext("2d")
+        ctx.fillStyle = color
+        ctx.fillRect(0, 0, width, height)
+    }
+    return canvas
+}
+
+function newCanvasFromSrc(src) {
+    const canvas = document.createElement("canvas")
+    const img = document.createElement("img")
+    img.onload = () => {
+        canvas.width = img.width
+        canvas.height = img.height
+        canvas.getContext("2d").drawImage(img, 0, 0, canvas.width, canvas.height)
+        canvas.loaded = true
+    }
+    img.onerror = console.error
+    img.src = src
+    // _enrichCanvas(canvas)
+    return canvas
+}
+
+function cloneCanvas(canvas, kwargs) {
+    const flipX = (kwargs && kwargs.flipX) || false
+    const flipY = (kwargs && kwargs.flipY) || false
+    const scaleX = (kwargs && kwargs.scaleX) || 1
+    const scaleY = (kwargs && kwargs.scaleY) || 1
+    const numCol = (kwargs && kwargs.col && kwargs.col[0]) || 0
+    const nbCols = (kwargs && kwargs.col && kwargs.col[1]) || 1
+    const numRow = (kwargs && kwargs.row && kwargs.row[0]) || 0
+    const nbRows = (kwargs && kwargs.row && kwargs.row[1]) || 1
+    const dx = (kwargs && kwargs.dx) || 0
+    const dy = (kwargs && kwargs.dy) || 0
+    const width = (kwargs && kwargs.width) || canvas.width * scaleX / nbCols
+    const height = (kwargs && kwargs.height) || canvas.height * scaleY / nbRows
+    const res = document.createElement("canvas")
+    assign(res, { width, height })
+    const ctx = res.getContext("2d")
+    ctx.save()
+    if(flipX) {
+        ctx.translate(width, 0)
+        ctx.scale(-1, 1)
+    }
+    if(flipY) {
+        ctx.translate(0, height)
+        ctx.scale(1, -1)
+    }
+    if(numCol !== 0 || dx !== 0) ctx.translate(dx - width * numCol, 0)
+    if(numRow !== 0 || dy !== 0) ctx.translate(0, dy - height * numRow)
+    if(scaleX !== 1) ctx.scale(scaleX, 1)
+    if(scaleY !== 1) ctx.scale(1, scaleY)
+    ctx.drawImage(canvas, 0, 0)
+    ctx.restore()
+    return res
+}
+
+function colorizeCanvas(canvas, color) {
+    const { width, height } = canvas
+    const colorCanvas = newCanvas(width, height, color)
+    const colorCtx = colorCanvas.getContext("2d")
+    colorCtx.globalCompositeOperation = "destination-in"
+    colorCtx.drawImage(canvas, 0, 0, width, height)
+    const ctx = canvas.getContext("2d")
+    ctx.save()
+    ctx.globalCompositeOperation = "color"
+    ctx.drawImage(colorCanvas, 0, 0, width, height)
+    ctx.restore()
+}
+
+function addCanvas(canvas, canvas2, x=0, y=0) {
+    const ctx = canvas.getContext("2d")
+    ctx.drawImage(canvas2, x, y)
+}
+
+function getHitBox(obj) {
+    if(obj.getHitBox) return obj.getHitBox()
+    if(obj.getBoundingClientRect) return obj.getBoundingClientRect()
+    const { x, y, width = 0, height = 0 } = obj
+    return {
+        left: x - width / 2,
+        top: y - height / 2,
+        width,
+        height,
+    }
+}
+
+function checkHit(obj1, obj2) {
+    const { left: l1, top: t1, width: w1, height: h1 } = getHitBox(obj1)
+    const { left: l2, top: t2, width: w2, height: h2 } = getHitBox(obj2)
+    return l1 < l2 + w2 && l2 < l1 + w1 && t1 < t2 + h2 && t2 < t1 + h1
+}
+
+
+export {
+    Group,
+    GameAudio,
+    addTo,
+    urlAbsPath,
+    fitTwoToEl,
+    newPointer,
+    addToLoads,
+    newCanvas,
+    newCanvasFromSrc,
+    cloneCanvas,
+    colorizeCanvas,
+    addCanvas,
+    checkAllLoadsDone,
+    checkHit,
+}
