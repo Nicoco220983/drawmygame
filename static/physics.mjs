2d0ec325a4f1e756aadfb29610f20581034af812
Nicolas CARREZ <nicolas.carrez@gmail.com>
2025-01-08 16:58:01 +0100

    Redesign physic engine whith pur SAT algo

--- static/physics.mjs
+++ static/physics.mjs
@@ -1,14 +1,10 @@
-const { abs, floor, ceil, min, max, pow, sqrt, atan2, PI, random } = Math
+const { abs, floor, ceil, min, max, pow, sqrt, hypot, atan2, PI, random } = Math
 const { assign } = Object
 
 const FLOAT_PRECISION_CORRECTION = .00001
 
-const NO_COLLISION_WITH_SPEED = 0
-const NO_COLLISION_WITHOUT_SPEED = 1
-const COLLISION_WITHOUT_SPEED = 2
 
-
-const colRes = {}, projRes = {}, fixRes = {}
+const colRes = {}, wallColRes = {}, projRes = {}, fixRes = {}, detectColRes = {}, colWalls = new Set()
 
 export default class PhysicsEngine {
     constructor(game) {
@@ -76,11 +72,13 @@ export default class PhysicsEngine {
         assign(physicData, { minX, minY, maxX, maxY, sMinX, sMinY, sMaxX, sMaxY })
         // normals
         const normals = physicData.normals ||= []
+        normals.length = 0
         for(let i=0; i<polygon.length/2; i+=2) {  // /2 because of symetry
             const edgeX = polygon[i+2] - polygon[i]
             const edgeY = polygon[i+3] - polygon[i+1]
-            if(edgeX == 0 || edgeY == 0) continue  // case already managed by min/max
-            normals.push(edgeY, -edgeX)
+            //if(edgeX == 0 || edgeY == 0) continue  // case already managed by min/max
+            const size = max(1, hypot(edgeX, edgeY))
+            normals.push(edgeY/size, -edgeX/size)
         }
         if(dx != 0 && dy != 0) normals.push(dy, -dx)
     }
@@ -95,54 +93,56 @@ export default class PhysicsEngine {
             const entOrigDx = entOrigSpdX * dt, entOrigDy = entOrigSpdY * dt
             if(ent.undergoWalls && (entOrigSpdX != 0 || entOrigSpdY != 0)) {
                 const entOrigD = dist(entOrigDx, entOrigDy) * dt
+                colWalls.clear()
                 while(remD > 0) {
-                    colRes.d2 = null
+                    colRes.time = Infinity
                     this.initEntity(dt*remD, ent)
                     const entData = ent.physicData
                     const {
                         minX: entMinX, minY: entMinY, maxX: entMaxX, maxY: entMaxY,
                         sMinX: entSMinX, sMinY: entSMinY, sMaxX: entSMaxX, sMaxY: entSMaxY,
                     } = entData
-                    let fixed = false
                     for(let wall of walls) {
-                        let col = COLLISION_WITHOUT_SPEED
+                        if(colWalls.has(wall)) continue
                         const wallData = wall.physicData
                         // quick filtering
-                        if(entSMinX > wallData.sMaxX || entSMaxX < wallData.sMinX || entSMinY > wallData.sMaxY || entSMaxY < wallData.sMinY) return
-                        if(entMinX > wallData.maxX || entMaxX < wallData.minX || entMinY > wallData.maxY || entMaxY < wallData.minY) col = NO_COLLISION_WITHOUT_SPEED
-                        // less quick but more exact filtering
-                        col = min(col, detectCollision(col, entData, wallData))
-                        if(col == NO_COLLISION_WITH_SPEED) continue
-                        // if(col == COLLISION_WITHOUT_SPEED && fixCollision(entData, wallData, fixRes)) {
-                        //     fixed = true; nbFixes += 1; break
-                        // }
-                        // determine closest collision point
-                        determineClosestCollisionPoint(col, entData, wallData, colRes)
+                        if(entSMinX > wallData.sMaxX || entSMaxX < wallData.sMinX || entSMinY > wallData.sMaxY || entSMaxY < wallData.sMinY) continue
+                        //if(entMinX > wallData.maxX || entMaxX < wallData.minX || entMinY > wallData.maxY || entMaxY < wallData.minY) col = NO_COLLISION_WITHOUT_SPEED
+                        // detect collision
+                        detectCollisionTime(entData, wallData, wallColRes)
+                        if(wallColRes.time == Infinity) continue
+                        if(wallColRes.time < colRes.time) assign(colRes, wallColRes)
+                        if(colRes.time == 0) break
                     }
-                    if(nbFixes==3) remD = 0
-                    if(fixed) continue
-                    if(colRes.d2 !== null) {
-                        nbCollisions += 1
-                        const { speedX: entSpdX, speedY: entSpdY } = ent
-                        const  { dx: entDx, dy: entDy } = entData
-                        const { d2: colD2, edgeX: colEdgeX, edgeY: colEdgeY } = colRes
-                        const entD = dist(entDx, entDy), colD = sqrt(colD2), df = colD / entD
+                    if(colRes.time == Infinity) break
+                    colWalls.add(colRes.wall)
+                    nbCollisions += 1
+                    let { speedX: entSpdX, speedY: entSpdY } = ent
+                    const  { dx: entDx, dy: entDy } = entData
+                    const { time: colTime, dist: colDist, distFixSign: colDistFixSign, normalX: colNormalX, normalY: colNormalY } = colRes
+                    if(colTime > 0) {
+                        const entD = hypot(entDx, entDy), colD = entD * colTime
                         // move
-                        ent.x += entDx * df
-                        ent.y += entDy * df
+                        ent.x += entDx * colTime
+                        ent.y += entDy * colTime
                         // move away entity a bit from wall to avoid float precision error
                         ent.x -= (entDx / entD) * FLOAT_PRECISION_CORRECTION
                         ent.y -= (entDy / entD) * FLOAT_PRECISION_CORRECTION
                         // compute remaining speed
-                        projection(entSpdX, entSpdY, colEdgeX, colEdgeY, projRes)
+                        projection(entSpdX, entSpdY, colNormalY, -colNormalX, projRes)
                         ent.speedX = projRes.x; ent.speedY = projRes.y
                         // stop collisions detection condition
                         remD -= colD / entOrigD
-                        if(dist(ent.speedX, ent.speedY) * remD < 1) remD = 0
-                        if(nbCollisions==3) remD = 0
-                    } else break
+                        if(hypot(ent.speedX, ent.speedY) * remD < 1) remD = 0
+                    } else {
+                        // static collision detected: fix position
+                        ent.x += colNormalX * colDistFixSign * (colDist + FLOAT_PRECISION_CORRECTION)
+                        ent.y += colNormalY * colDistFixSign * (colDist + FLOAT_PRECISION_CORRECTION)
+                    }
+                    if(nbCollisions==5) remD = 0
                 }
             }
+            //console.log("TMP nbCollisions", nbCollisions)
             if(remD > 0) {
                 ent.x += ent.speedX * dt * remD
                 ent.y += ent.speedY * dt * remD
@@ -158,124 +158,78 @@ export default class PhysicsEngine {
     }
 }
 
-function detectCollision(col, physicData1, physicData2) {
-    const { normals: normals1 } = physicData1
-    const { normals: normals2 } = physicData2
-    for(let i=0; i<normals1.length; i+=2) {
-        col = min(col, detectCollisionOnAxis(physicData1, physicData2, normals1[i], normals1[i+1]))
-        if(col == NO_COLLISION_WITH_SPEED) return NO_COLLISION_WITH_SPEED
-    }
-    for(let i=0; i<normals2.length; i+=2) {
-        col = min(col, detectCollisionOnAxis(physicData1, physicData2, normals2[i], normals2[i+1]))
-        if(col == NO_COLLISION_WITH_SPEED) return NO_COLLISION_WITH_SPEED
-    }
-    return col
+// Fonction principale pour détecter le moment de collision
+function detectCollisionTime(physicData1, physicData2, res) {
+    res.time = 0
+    res.dist = Infinity
+    res.distFixSign = 0
+    res.normalX = null
+    res.normalY = null
+    _detectCollisionTime(physicData1, physicData2, physicData1.normals, res)
+    if(res.time == Infinity) return
+    _detectCollisionTime(physicData2, physicData1, physicData2.normals, res)
 }
 
-const resProj1 = {}, resProj2 = {}
-function detectCollisionOnAxis(physicData1, physicData2, ax, ay) {
-    projectOnAxis(physicData1, ax, ay, resProj1)
-    projectOnAxis(physicData2, ax, ay, resProj2)
-    if(resProj1.sMax < resProj2.sMin || resProj2.sMax < resProj1.sMin) return NO_COLLISION_WITH_SPEED
-    if(resProj1.max < resProj2.min || resProj2.max < resProj1.min) return NO_COLLISION_WITHOUT_SPEED
-    return COLLISION_WITHOUT_SPEED
+const resProj1 = {}, resProj2 = {}, resOverlapTime = {}
+function _detectCollisionTime(physicData1, physicData2, normals, res) {
+    const { polygon: poly1, dx: dx1, dy: dy1 } = physicData1
+    const { polygon: poly2, dx: dx2, dy: dy2 } = physicData2
+    for(let i=0; i<normals.length; i+=2) {
+        const ax = normals[i], ay = normals[i+1]
+        projectPolygonOnAxis(poly1, ax, ay, resProj1) // TODO: cache me
+        projectPolygonOnAxis(poly2, ax, ay, resProj2)
+        getOverlapTime(resProj1, resProj2, dx1-dx2, dy1-dy2, ax, ay, resOverlapTime)
+        const { time: colTime, dist: colDist, distFixSign: colDistFixSign } = resOverlapTime
+        if(colTime < res.time) continue
+        if(colTime == 0 && colDist > res.dist) return
+        res.time = colTime
+        res.dist = colDist
+        res.distFixSign = colDistFixSign
+        res.normalX = ax
+        res.normalY = ay
+        if(colTime == Infinity) break
+    }
 }
 
-function projectOnAxis(physicData, ax, ay, res) {
-    const { dx, dy, polygon } = physicData
-    const hasSpeed = dx!=0 || dy!=0
-    for(let i=0; i<polygon.length; i+=2) {
+// Fonction pour trouver le temps de chevauchement des projections sur un axe
+function getOverlapTime(proj1, proj2, spdX, spdY, ax, ay, res) {
+
+    const dist12 = proj1.max - proj2.min, dist21 = proj2.max - proj1.min
+    const colDist = min(dist12, dist21)
+    res.dist = colDist
+    res.distFixSign = (dist12 < dist21) ? 1 : -1
+    if(colDist >= 0) {
+        // static collision detected
+        res.time = 0
+        return
+    }
+
+    const relSpdProj = spdX * ax + spdY * ay
+    if (relSpdProj === 0) {
+        // static without collision
+        res.time = Infinity
+        return
+    }
+
+    const t1 = (proj2.min - proj1.max) / relSpdProj
+    const t2 = (proj2.max - proj1.min) / relSpdProj
+    const tEnter = min(t1, t2), tExit = max(t1, t2)
+    if(tExit < 0 || tEnter > 1) res.time = Infinity
+    else res.time = tEnter
+}
+
+// Fonction pour projeter un polygone sur un axe
+function projectPolygonOnAxis(polygon, ax, ay, res) {
+    const polyLen = polygon.length
+    let pmin = Infinity, pmax = -Infinity
+    for(let i=0; i<polyLen; i+=2) {
         const x = polygon[i], y = polygon[i+1]
-        const p = x * ax + y * ay
-        if(i==0) {
-            res.min = res.sMin = p
-            res.max = res.sMax = p
-        } else {
-            res.min = min(p, res.min)
-            res.max = max(p, res.max)
-            if(hasSpeed) {
-                const p2 = (x + dx) * ax + (y + dy) * ay
-                res.sMin = min(p2, res.sMin)
-                res.sMax = max(p2, res.sMax)
-            } else {
-                res.sMin = res.min
-                res.sMax = res.max
-            }
-        }
+        const proj = x * ax + y * ay
+        pmin = min(pmin, proj)
+        pmax = max(pmax, proj)
     }
-}
-
-const centerRes1 = {}, centerRes2 = {}, interRes1 = {}, interRes2 = {}
-function fixCollision(physicData1, physicData2, res) {
-    const _getCenter = function(physicData, centerRes) {
-        centerRes.x = 0; centerRes.y = 0
-        const poly = physicData.polygon
-        for(let i=0; i<poly.length; i+=2) {
-            centerRes.x += poly[i]
-            centerRes.y += poly[i+1]
-        }
-        centerRes.x /= poly.length / 2
-        centerRes.y /= poly.length / 2
-    }
-    _getCenter(physicData1, centerRes1)
-    _getCenter(physicData2, centerRes2)
-    const _getIntersection = function(physicData, interRes) {
-        interRes.x = null; interRes.y = null
-        const poly = physicData.polygon, polyLen = poly.length
-        const polyLen2 = (polyLen > 4) ? polyLen : (polyLen -2)  // optim for segments
-        for(let i=0; i<polyLen2; i+=2) {
-            if(intersection(poly[i], poly[i+1], poly[(i+2)%polyLen], poly[(i+3)%polyLen], centerRes1.x, centerRes1.y, centerRes2.x, centerRes2.y, interRes))
-                break
-        }
-    }
-    _getIntersection(physicData1, interRes1)
-    const _interRes1 = (interRes1.x !== null) ? interRes1 : centerRes1
-    _getIntersection(physicData2, interRes2)
-    const _interRes2 = (interRes2.x !== null) ? interRes2 : centerRes2
-    res.x = _interRes2.x - _interRes1.x
-    res.y = _interRes2.y - _interRes1.y
-}
-
-const interRes = {}
-function determineClosestCollisionPoint(col, physicData1, physicData2, res) {
-    const _determineClosestCollisionPoint = (data1, data2, dir) => {
-        const { polygon: poly1, dx: dx1, dy: dy1 } = data1
-        const { polygon: poly2, dx: dx2, dy: dy2 } = data2
-        // loop on data1 extremities
-        for(let i1=0; i1<poly1.length; i1+=2) {
-            const x1 = poly1[i1], y1 = poly1[i1+1]
-            // loop on data2 edges
-            const polyLen2 = poly2.length, polyLen2ToLoop = polyLen2 <=4 ? 1 : polyLen2
-            for(let i2=0; i2<polyLen2ToLoop; i2+=2) {
-                const x21 = poly2[i2], y21 = poly2[i2+1], x22 = poly2[(i2+2)%polyLen2], y22 = poly2[(i2+3)%polyLen2]
-                const dx = dx1 - dx2, dy = dy1 - dy2
-                if(intersection(x1, y1, x1+dx, y1+dy, x21, y21, x22, y22, interRes)) {
-                    const interDx = (interRes.x - x1) * dir, interDy = (interRes.y - y1) * dir
-                    const d2 = dist2(interDx, interDy)
-                    if(res.d2 === null || res.d2 > d2) {
-                        res.d2 = d2
-                        res.edgeX = x21 - x22
-                        res.edgeY = y21 - y22
-                    }
-                }
-            }
-        }
-    }
-    _determineClosestCollisionPoint(physicData1, physicData2, 1)
-    _determineClosestCollisionPoint(physicData2, physicData1, -1)
-}
-
-function intersection(x1, y1, x2, y2, x3, y3, x4, y4, res) {
-    const denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
-    if (denom === 0) return false  // parallel case
-    const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / denom
-    const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / denom
-    if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
-        res.x = x1 + t * (x2 - x1)
-        res.y = y1 + t * (y2 - y1)
-        return true
-    }
-    return false
+    res.min = pmin
+    res.max = pmax
 }
 
 function projection(x1, y1, x2, y2, res) {
