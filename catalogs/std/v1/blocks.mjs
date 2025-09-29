a25ba1688d10a19faf4de3bb2c1cae8e0da9955e
Nicoco220983 <nicolas.carrez@gmail.com>
2025-09-29 11:02:43 +0200

    Make Door as a block

--- catalogs/std/v1/blocks.mjs
+++ catalogs/std/v1/blocks.mjs
@@ -40,3 +40,47 @@ export class GrassBlock extends Block {
         return GrassImg
     }
 }
+
+
+const DoorImg = CATALOG.registerImage("/static/catalogs/std/v1/assets/blocks/door.png")
+const DoorSpriteSheet = new SpriteSheet(CATALOG.registerImage("/static/catalogs/std/v1/assets/blocks/door_spritesheet.png"), 2, 1)
+
+@CATALOG.registerObject("door", {
+    label: "Door",
+    icon: DoorImg,
+    showInBuilder: true,
+})
+@LinkReaction.add("reactToggle", { label:"toggle", isDefault: true })
+@PhysicsMixin.add({
+    canMove: false,
+    canBlock: true,
+    checkBlockAnyway: true,
+})
+@StateBool.define("closed", { default: true, showInBuilder: true })
+@Category.append("engine")
+export class Door extends Block {
+
+    init(kwargs) {
+        super.init(kwargs)
+        this.origClosed = this.closed
+        this.lastBlockIt = -Infinity
+    }
+
+    reactToggle(resp) {
+        const shouldBeClosed = (resp.value >= .5) ? (!this.origClosed) : this.origClosed
+        this.closed = shouldBeClosed && (this.closed || this.lastBlockIt < this.scene.iteration)
+    }
+
+    update() {
+        super.update()
+        this.canBlock = this.closed
+    }
+
+    onBlock(obj) {
+        this.lastBlockIt = this.scene.iteration
+    }
+
+    getBaseImg() {
+        return DoorSpriteSheet.get(this.closed ? 0 : 1)
+    }
+}
