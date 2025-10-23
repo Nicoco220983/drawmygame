febadd894717c8ccc6ea3651bedb45f5f05d23d1
Nicoco220983 <nicolas.carrez@gmail.com>
2025-10-23 16:27:15 +0200

    Add JSDoc toaudio.mjs and graphics.mjs

--- core/v1/audio.mjs
+++ core/v1/audio.mjs
@@ -1,16 +1,32 @@
 const IS_SERVER_ENV = (typeof window === 'undefined')
 
 
+/**
+ * The audio engine of the game.
+ * It is responsible for playing sounds.
+ * @param {Game} game The game instance.
+ */
 export class AudioEngine {
     constructor(game) {
         if(IS_SERVER_ENV) return
         this.game = game
     }
 
+    /**
+     * Returns the audio context.
+     * @returns {AudioContext} The audio context.
+     */
     getContext() {
         return this.context ||= new (window.AudioContext || window.webkitAudioContext)()
     }
 
+    /**
+     * Plays a sound.
+     * @param {Audio} aud The audio to play.
+     * @param {number} volume The volume of the sound.
+     * @param {boolean} loop Whether to loop the sound.
+     * @returns {AudioBufferSourceNode} The sound source.
+     */
     async playSound(aud, volume = 1.0, loop = false) {
         if(IS_SERVER_ENV) return
 
--- core/v1/graphics.mjs
+++ core/v1/graphics.mjs
@@ -4,21 +4,38 @@ import * as utils from './utils.mjs'
 const { urlAbsPath, checkHit, sumTo, newCanvas, addCanvas, cloneCanvas, colorizeCanvas, newDomEl, addNewDomEl, importJs } = utils
 
 
+/**
+ * The graphical properties of an object.
+ * It contains all the information needed to draw an object.
+ * @param {object} kwargs The properties to assign.
+ */
 export class GraphicsProps {
 
     static {
         assign(this.prototype, {
+            /** @type {string} */
             color: null,
+            /** @type {HTMLImageElement} */
             img: null,
+            /** @type {number} */
             x: 0,
+            /** @type {number} */
             y: 0,
+            /** @type {number} */
             width: 50,
+            /** @type {number} */
             height: 50,
+            /** @type {number} */
             dirX: 1,
+            /** @type {number} */
             dirY: 1,
+            /** @type {number} */
             angle: 0,
+            /** @type {number} */
             order: 0,
+            /** @type {number} */
             visibility: 1,
+            /** @type {string} */
             colorize: null,
         })
     }
@@ -27,17 +44,30 @@ export class GraphicsProps {
         assign(this, kwargs)
     }
 
+    /**
+     * Draws the object.
+     * @param {GraphicsEngine} drawer The graphics engine to use.
+     */
     draw(drawer) {
         drawer.draw(this)
     }
 }
 
 
+/**
+ * The graphics engine of the game.
+ * It is responsible for drawing objects on the screen.
+ * @param {Scene} scn The scene to draw.
+ */
 export class GraphicsEngine {
     constructor(scn) {
         this.scene = scn
     }
 
+    /**
+     * Draws the objects.
+     * @param  {...GraphicsProps} propss The properties of the objects to draw.
+     */
     draw(...propss) {
         propss.sort((p1, p2) => p1.order - p2.order)
         const scn = this.scene, can = scn.canvas
@@ -59,6 +89,19 @@ export class GraphicsEngine {
         }
     }
 
+    /**
+     * Transforms an image.
+     * It can resize, flip, rotate, and colorize an image.
+     * @param {HTMLImageElement} baseImg The image to transform.
+     * @param {number} width The new width of the image.
+     * @param {number} height The new height of the image.
+     * @param {number} dirX The direction of the x-axis.
+     * @param {number} dirY The direction of the y-axis.
+     * @param {number} angle The angle of rotation.
+     * @param {number} visibility The visibility of the image.
+     * @param {string} colorize The color to use for colorization.
+     * @returns {HTMLCanvasElement} The transformed image.
+     */
     transformImg(baseImg, width, height, dirX, dirY, angle, visibility, colorize) {
         width = round(width)
         height = round(height)
